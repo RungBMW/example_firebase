ea5211d6ae4fdb15379ffd12487405946d6d74ba
ThyRung <kanrung16@gmail.com>
2020-06-19 17:36:25 +0700

    display dialog

--- lib/display on app/display.dart
+++ lib/display on app/display.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:example_firebase/display%20on%20app/user.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'user.dart';
+
+
 
 class Display extends StatefulWidget {
 
@@ -17,14 +25,18 @@ class _DisplayState extends State<Display> {
   String collectionName ="Users";
   bool isEditing = false;
   User curUser;
+  String imagelink;
+  File imageFile;
+  DocumentSnapshot data;
 
+  // Get User
   getUsers(){
-    //
     return Firestore.instance.collection(collectionName).snapshots();
   }
 
+  // Add User
   addUser(){
-   User user = User(name: controller.text);
+   User user = User(name: controller.text, url: imagelink);
    try {
      Firestore.instance.runTransaction((Transaction transaction) async {
        await Firestore.instance.collection(collectionName).document().setData(user.toJson());
@@ -34,10 +46,11 @@ class _DisplayState extends State<Display> {
    }
   }
 
+  // Add
   add(){
     if(isEditing){
       // update
-      update(curUser, controller.text);
+      update(curUser, controller.text,);
       setState(() {
         isEditing = false;
       });
@@ -47,18 +60,21 @@ class _DisplayState extends State<Display> {
     controller.text = '';
   }
 
-  update( User user, String newName){
+  // Update
+  update(User user, String newName){
     Firestore.instance.runTransaction((Transaction transaction) async {
-      await transaction.update(user.reference, {'name': newName});
+      await transaction.update(user.reference, {'name': newName,});
     });
   }
 
+  // Delete
   delete(User user){
     Firestore.instance.runTransaction((Transaction transaction) async {
       await transaction.delete(user.reference);
     });
   }
 
+  // Build Body
   Widget buildBody(BuildContext context){
     return StreamBuilder<QuerySnapshot>(
       stream: getUsers(),
@@ -76,16 +92,27 @@ class _DisplayState extends State<Display> {
     );
   }
 
+  Widget _dicideImageView(BuildContext context, DocumentSnapshot data){
+    if(imageFile == null){
+      return Text('No Image Select');
+    }else{
+      return Image.file(imageFile, width: 100,height: 100,);
+    }
+  }
+
+  // Build List
   Widget buildList(BuildContext context, List<DocumentSnapshot> snapshot){
     return ListView(
       children: snapshot.map((data) => buildListItem(context, data)).toList(),
     );
   }
 
+  // Build ListItem
   Widget buildListItem(BuildContext context, DocumentSnapshot data){
+
     final record = User.fromSnapshot(data);
     return Padding(
-      key: ValueKey(record.name),
+      key: ValueKey(record.name,),
       padding: EdgeInsets.symmetric(vertical: 10),
       child: Container(
         decoration: BoxDecoration(
@@ -93,6 +120,16 @@ class _DisplayState extends State<Display> {
           borderRadius: BorderRadius.circular(5),
         ),
         child: ListTile(
+          leading: GestureDetector(
+            child: Hero(
+              tag: 'image-one-tag',
+              child: CircleAvatar (
+                backgroundImage: NetworkImage(record.url),
+                radius: 22,
+              ),
+            ),
+            onTap: () => _showImage(context,data),
+          ),
           title: Text(record.name),
           trailing: IconButton(
             icon: Icon(Icons.delete),
@@ -101,15 +138,46 @@ class _DisplayState extends State<Display> {
               delete(record);
             },
           ),
-          onTap: (){
+          onTap: () {
             // update
             setUpdateUI(record);
+            showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: const Text('Information Yourself'),
+                content: Padding(
+                  padding: EdgeInsets.all(0),
+                  child: Column(
+                    children: <Widget>[
+                      FlatButton(
+                          child: Text('Selected Image'),
+                          onPressed: () async {
+                            imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+                            print("Image File"+ imageFile.path);
+                            //  _showPhotos(context,imageFile);
+                          }
+                      ),
+                      SizedBox(height: 10,),
+                      TextFormField(
+                        controller: controller,
+                        decoration: InputDecoration(
+                          labelText: "Name", hintText: "Enter name",
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      button(),
+                    ],
+                  ),
+                ),
+              ),
+            );
           },
         ),
       ),
     );
   }
 
+  // Set UpdateUI
   setUpdateUI(User user){
     controller.text = user.name;
     setState(() {
@@ -119,6 +187,7 @@ class _DisplayState extends State<Display> {
     });
   }
 
+  // Button for Add
   button(){
     return SizedBox(
       width: double.infinity,
@@ -129,9 +198,23 @@ class _DisplayState extends State<Display> {
           setState(() {
             showTextField = false;
           });
+          Navigator.of(context).pop();
         },
       ),
     );
+  }
+
+  // Show Image Display on Screen
+  void _showImage(BuildContext context, DocumentSnapshot data) {
+    final record = User.fromSnapshot(data);
+    Navigator.of(context).push(MaterialPageRoute (builder: (context) => Scaffold(
+      body: Center(
+        child: Hero(
+            tag: 'image-one-tag',
+            child:  Image.network(record.url)
+        ),
+      ),
+    )));
   }
 
   @override
@@ -146,6 +229,54 @@ class _DisplayState extends State<Display> {
             onPressed: (){
               setState(() {
                 showTextField =! showTextField;
+                showDialog<String>(
+                    context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Information Yourself'),
+                    content: Padding(
+                      padding: EdgeInsets.all(0),
+                      child: Column(
+                        children: <Widget>[
+                          // Show Image
+                          _dicideImageView(context,data),
+
+                          FlatButton(
+                              child: Text('Selected Image'),
+                              onPressed: () async {
+                                imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+                                print("Image File" +imageFile.path);
+
+                                // Add Image into Storage
+                                FirebaseStorage fs = FirebaseStorage.instance;
+                                StorageReference rootSref = fs.ref();
+                                Random random = Random.secure();
+                                String choice = random.nextInt(1000).toString();
+                                StorageReference pictureSref = rootSref.child("pictures").child("image_"+choice);
+
+                                // Image in Storage into Database
+                                pictureSref.putFile(imageFile).onComplete.then((storageTask) async {
+                                  String link = await storageTask.ref.getDownloadURL();
+                                  print("Uploaded");
+                                  setState(() {
+                                    imagelink = link;
+                                  });
+                                });
+                              }
+                          ),
+                          SizedBox(height: 10,),
+                          TextFormField(
+                            controller: controller,
+                            decoration: InputDecoration(
+                              labelText: "Name", hintText: "Enter name",
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          button(),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
               });
             },
           ),
@@ -159,21 +290,11 @@ class _DisplayState extends State<Display> {
           children: <Widget>[
             showTextField? Column(
               crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                TextFormField(
-                  controller: controller,
-                  decoration: InputDecoration(
-                    labelText: "Name", hintText: "Enter name",
-                  ),
-                ),
-                SizedBox(height: 10,),
-                button(),
-              ],
-            ) : Container(),
-            SizedBox(height: 20,),
+              mainAxisAlignment: MainAxisAlignment.start,) : Container(),
+            SizedBox(height: 10,),
             Text("USERS", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),),
-            SizedBox(height: 20,),
+            SizedBox(height: 10,),
+            Column(),
             Flexible(
               child: buildBody(context),
             ),
--- lib/display on app/user.dart
+++ lib/display on app/user.dart
@@ -3,16 +3,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class User{
 
   String name;
+  // url is image network
+  String url;
   DocumentReference reference;
 
-  User({this.name});
+  User({this.name, this.url});
 
   User.fromMap(Map<String, dynamic> map, {this.reference}){
     name = map["name"];
+    url = map["image"];
   }
 
   User.fromSnapshot(DocumentSnapshot snapshot) : this.fromMap(snapshot.data, reference: snapshot.reference);
   toJson(){
-    return{'name': name};
+    return{
+      'name': name,
+      'image': url,
+    };
   }
 }
